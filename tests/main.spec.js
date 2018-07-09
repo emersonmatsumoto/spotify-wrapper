52b72f203b4bf5317acb6045c19d712f54658453
Emerson Katsumi Matsumoto <japinha02@gmail.com>
2018-07-09 13:47:17 -0300

    Promise

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -50,16 +50,16 @@ describe('Main', () => {
 
       expect(fetchedStub).to.have.been.calledOnce;
     });
-    
+
     it('should receive the correct url to fetch', () => {
       context('passing one type', () => {
         const artists = search('Incubus', 'artist');
-  
+
         expect(fetchedStub).to.have.been
           .calledWith('https://api.spotify.com/v1/search?q=Incubus&type=artist');
-  
+
         const albums = search('Incubus', 'album');
-  
+
         expect(fetchedStub).to.have.been
           .calledWith('https://api.spotify.com/v1/search?q=Incubus&type=album');
       });
