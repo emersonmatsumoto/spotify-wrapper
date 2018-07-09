ac67851e4f45ee2472875a20c3d4461fca463193
Emerson Katsumi Matsumoto <japinha02@gmail.com>
2018-07-09 11:10:23 -0300

    Refactoring fetchedStub

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -34,26 +34,40 @@ describe('Main', () => {
   });
 
   describe('Generic search', () => {
+    let fetchedStub;
+    beforeEach(() => {
+      fetchedStub = sinon.stub(global, 'fetch');
+    });
+
+    afterEach(() => {
+      fetchedStub.restore();
+    });
+
     it('should call fetch function', () => {
-      const fetchedStub = sinon.stub(global, 'fetch');
       const artists = search();
 
       expect(fetchedStub).to.have.been.calledOnce;
-
-      fetchedStub.restore();
     });
     
     it('should receive the correct url to fetch', () => {
-      const fetchedStub = sinon.stub(global, 'fetch');
-      const artists = search('Incubus', 'artist');
+      context('passing one type', () => {
+        const artists = search('Incubus', 'artist');
+  
+        expect(fetchedStub).to.have.been
+          .calledWith('https://api.spotify.com/v1/search?q=Incubus&type=artist');
+  
+        const albums = search('Incubus', 'album');
+  
+        expect(fetchedStub).to.have.been
+          .calledWith('https://api.spotify.com/v1/search?q=Incubus&type=album');
+      });
 
-      expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=Incubus&type=artist');
+      context('passing more than one type', () => {
+        const artistsAndAlbums = search('Incubus', ['artists', 'album']);
 
-      const albums = search('Incubus', 'albums');
-
-      expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=Incubus&type=albums');
-
-      fetchedStub.restore();
+        expect(fetchedStub).to.have.been
+          .calledWith('https://api.spotify.com/v1/search?q=Incubus&type=artist,album');
+      });
     });
   });
 });
