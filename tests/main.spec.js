bca35568f70d32a4dfcec17b9369f59d7123a3ca
Emerson Katsumi Matsumoto <japinha02@gmail.com>
2018-07-09 10:56:26 -0300

    Test generic search

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,8 +1,17 @@
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import sinonStubPromise from 'sinon-stub-promise';
 import { search, searchAlbums, searchArtists, searchTracks, searchPlaylists } from '../src/main';
 
+chai.use(sinonChai);
+sinonStubPromise(sinon);
+
+global.fetch = require('node-fetch');
+
+
 describe('Main', () => {
-  describe('smoke tests', () => {
+  describe('Smoke tests', () => {
     it('should exist the search method', () => {
       expect(search).to.exist;
     });
@@ -21,6 +30,30 @@ describe('Main', () => {
 
     it('should exist the searchPlaylists method', () => {
       expect(searchPlaylists).to.exist;
+    });
+  });
+
+  describe('Generic search', () => {
+    it('should call fetch function', () => {
+      const fetchedStub = sinon.stub(global, 'fetch');
+      const artists = search();
+
+      expect(fetchedStub).to.have.been.calledOnce;
+
+      fetchedStub.restore();
+    });
+    
+    it('should receive the correct url to fetch', () => {
+      const fetchedStub = sinon.stub(global, 'fetch');
+      const artists = search('Incubus', 'artist');
+
+      expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=Incubus&type=artist');
+
+      const albums = search('Incubus', 'albums');
+
+      expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=Incubus&type=albums');
+
+      fetchedStub.restore();
     });
   });
 });
